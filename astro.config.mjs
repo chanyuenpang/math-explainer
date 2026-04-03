71cc8d1f694541b4b23335e029fcb89d109021be
Yop <yop@example.com>
2026-04-03 16:08:38 +0800

    [ci] 添加 GitHub Pages 部署

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/math-explainer/',
   integrations: [react()],
   output: 'static',
   vite: {
